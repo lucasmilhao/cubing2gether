f929c9574574fb33bb843ff5630a40f36f28c84d
Lucas Milhão <lucasmilhao683@gmail.com>
2026-03-31 18:03:29 -0300

    Delete cubing2gether spring directory

--- cubing2gether spring/src/main/java/com/example/teste/repository/SolveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.teste.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.teste.model.Solve;
-
-public interface SolveRepository extends JpaRepository<Solve, Long>{
-    
-}
