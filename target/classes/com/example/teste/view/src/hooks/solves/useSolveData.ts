3b393380bf4ebba25722bae3558de668a64d55b8
Lucas Milhão <lucasmilhao683@gmail.com>
2026-03-31 18:06:09 -0300

    Add files via upload

---
+++ target/classes/com/example/teste/view/src/hooks/solves/useSolveData.ts
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import type { AxiosPromise } from "axios";
+import axios from "axios";
+
+const API = `http://localhost:8080`;
+
+interface SolveProps {
+    id : number,
+    tempo :  number,
+    scramble: string,
+    penalty : string,
+    userId: string
+}
+
+const fetchData = async () : AxiosPromise<SolveProps[]> => {
+    const token = localStorage.getItem("authToken");
+    
+    const response = await axios.get(`${API}/solves`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    return response;
+}
+
+export function useSolveData() {
+    return useQuery ({
+        queryFn: fetchData,
+        queryKey: ["solves-data"],
+        retry: 2
+    })
+    
+}
