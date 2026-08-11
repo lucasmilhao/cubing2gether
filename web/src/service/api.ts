9b5b3f36ff3ad923389292d4d9b04144f2bc0a0c
Lucas Milhão <lucasmilhao683@gmail.com>
2026-08-11 16:52:48 -0300

    Update API base URL to production environment

--- web/src/service/api.ts
+++ web/src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://cubing2gether.onrender.com",
     withCredentials: true
 });
 
